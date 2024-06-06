695011b594e084fabeaeb7046db1f7557ddd9c4f
sebtech974 <sebtech974@gmail.com>
2024-06-06 09:10:18 +0400

    maj images + components

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import Nav from '../components/header/nav';
 import Header from '../components/header/header';
 
+
 const Home = () => {
     return (
         <div>
         <Header />
         <Nav />
+       
+
         </div>
     );
 };
